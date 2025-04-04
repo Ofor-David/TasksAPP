ec5d87f20f723effc07b4691f8c5f65272b4aaef
Ofor-David <davidoforincloud@gmail.com>
2025-04-04 12:18:18 +0100

    add: Frontend, CORS

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,9 +1,22 @@
 import express from 'express'
 import { registerUser, loginUser, getUserProfile, deleteUser } from '../controllers/userController.js'
 import authMiddleware from '../middleware/authMiddleware.js'
+import passport from 'passport'
 const userRoute = express.Router()
 
+/* userRoute.get('/auth/google',
+    passport.authenticate('google', {
+        scope:
+            ['email', 'profile']
+    }
+    ));
 
+userRoute.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/auth/google/success',
+        failureRedirect: '/auth/google/failure'
+    }));
+     */
 userRoute.post('/register', registerUser)
 userRoute.post('/login', loginUser)
 userRoute.get('/profile', authMiddleware, getUserProfile)
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import taskRoute from '../routes/taskRoute.js';
 import userRoute from '../routes/userRoute.js';
 import logger from '../middleware/logger.js';
+import cors from 'cors';
 //consts
 const port = process.env.port
 const mongodb_url = process.env.mongodb_url
@@ -20,9 +21,20 @@ app.use(urlencoded({ extended: false }))
 //use middleware
 app.use(logger)
 
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Specify allowed methods
+    credentials: true,  // Allow credentials (e.g., cookies, authorization headers)
+};
+// Enable CORS for all origins (you can restrict to a specific domain for security)
+app.use(cors(corsOptions));
+
+
+
 //use routes
 app.use('/api/tasks', taskRoute)
 app.use('/api/users', userRoute)
+
 //Init server
 
 async function connectDB() {
